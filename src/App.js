d0df914929cae44a6c1d1dddac60e02268dbab96
Antonio Carlos Catani neto <cataninetoo@gmail.com>
2020-12-21 16:49:53 -0300

    [Feat] Initial components and containers creation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-function App() {
-  return (
-    <div>
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/Burger-Builder/BurgerBuilder';
 
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p></p>
+        </Layout>
+
+        <BurgerBuilder/>
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
