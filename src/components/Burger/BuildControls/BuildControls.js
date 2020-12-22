a3e53c2355d703d49da9d01d741ac5f5d5db88da
Antonio Carlos Catani neto <cataninetoo@gmail.com>
2020-12-22 00:37:06 -0300

    [Feat] Display burger price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -24,14 +24,15 @@ const controls = [
 const buildControls = (props) => {
     return (
         <div className={classes.BuildControls}>
+            <p>Burger price: <strong>US$ {props.price.toFixed(2)}</strong></p>
             {controls.map((value) => {
                 return <BuildControl
                     key={value.label}
                     label={value.label}
                     type={value.type}
                     disabled={props.disabled[value.type]}
-                    ingredientAdded={() => props.ingredientAdded(value.type)} 
-                    ingredientRemoved={() => props.ingredientRemoved(value.type)}/>;
+                    ingredientAdded={() => props.ingredientAdded(value.type)}
+                    ingredientRemoved={() => props.ingredientRemoved(value.type)} />;
             })}
         </div>
     )
