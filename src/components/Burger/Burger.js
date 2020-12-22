94546a31276b7cfb522b9ba2eb38c3e73e7aebd8
Antonio Carlos Catani neto <cataninetoo@gmail.com>
2020-12-21 21:08:48 -0300

    [Feat] Add message when there are no ingredients added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,18 +4,25 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
-        .map((igKey) => {
-            return [...Array(props.ingredients[igKey])].map((_, idx) => {
-               return <BurgerIngredient key={igKey + idx} type={igKey}></BurgerIngredient>
-            })
-        })
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+               return <BurgerIngredient key={igKey + i} type={igKey}/>;
+            });
+        }).reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+        console.log(transformedIngredients);
+
+        if  (transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please start adding ingredients!</p>;
+        }
     return (
         <div className={classes.Burger}>
 
-            <BurgerIngredient type='bread-top'></BurgerIngredient>
+            <BurgerIngredient type='bread-top'/>
             {transformedIngredients}
-            <BurgerIngredient type='bread-bottom'></BurgerIngredient>
+            <BurgerIngredient type='bread-bottom'/>
         </div>
     )
 }
