93aa0e91ace311a52b3624c815b2d6dceca3268d
Antonio Carlos Catani neto <cataninetoo@gmail.com>
2021-02-06 22:27:47 -0300

    [Feat] Change "Menu" text for a Hamburger Icon

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,7 +7,7 @@ import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 const toolbar = (props) => {
 
     return (
-        <header class={classes.Toolbar}>
+        <header className={classes.Toolbar}>
             <DrawerToggle clicked={props.drawerToggleClicked}/>
             <div className={classes.Logo}>
                 <Logo />
